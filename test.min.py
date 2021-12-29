d4e662935b4e14acb2dc03b0d3f8243aa403f2a9
Samuel Palmér <samuel.palmer@relevant-digital.com>
2021-12-29 20:30:37 +0100

    fix

--- test.min.py
+++ test.min.py
@@ -1,161 +1,147 @@
 import microbit as mb
-y=0
-s=1
-r=2
-z=1
-w=2
-class Inställningar:
- def __init__(a):
-  a.riktning=y
-  a.sväng=y
- def sätt(a,I,E):
-  a.riktning=I
-  a.sväng=E
- def sätt_från_annan(a,A):
-  a.sätt(A.riktning,A.sväng)
- def är_samma(a,A):
-  return a.riktning==A.riktning and a.sväng==A.sväng
- def bild(a):
-  if a.sväng==z:
-   return mb.Image.ARROW_W
-  if a.sväng==w:
-   return mb.Image.ARROW_E
-  return mb.Image.HEART
-def sp(m,d):
- if m.read_digital()!=d:
-  m.write_digital(d)
-def motsatt_sida(E):
- return w if E==z else w
-K=Inställningar()
+T=0
+r=mb.pin13
+y=mb.pin14
+l=mb.pin16
+V=mb.pin15
+IM=mb.Image
+class Inst:
+ def __init__(K):
+  K.sätt(T,T)
+ def sätt(K,C,S):
+  K.rik=C
+  K.sväng=S
+ def sätt_från_annan(K,N):
+  K.sätt(N.rik,N.sväng)
+ def är_samma(K,N):
+  return K.rik==N.rik and K.sväng==N.sväng
+ def bild(K):
+  if K.rik==y:
+   return IM.NO
+  if K.sväng==l:
+   return IM.ARROW_W
+  if K.sväng==V:
+   return IM.ARROW_E
+  return IM.HEART
+def sp(B,A):
+ A.write_digital(1 if B==A else 0)
+def motsatt_sida(S):
+ return V if S==l else l
+a=Inst()
 def flytta(ins):
- K.sätt_från_annan(ins)
- if ins.riktning==s:
-  sp(mb.pin13,1)
-  sp(mb.pin14,0)
- elif ins.riktning==r:
-  sp(mb.pin13,0)
-  sp(mb.pin14,1)
- else:
-  sp(mb.pin13,0)
-  sp(mb.pin14,0)
- if ins.sväng==z:
-  sp(mb.pin15,0)
-  sp(mb.pin16,1)
- elif ins.sväng==w:
-  sp(mb.pin15,1)
-  sp(mb.pin16,0)
- else:
-  sp(mb.pin15,0)
-  sp(mb.pin16,0)
+ a.sätt_från_annan(ins)
+ sp(ins.rik,r)
+ sp(ins.rik,y)
+ sp(ins.sväng,l)
+ sp(ins.sväng,V)
 class Öga:
- def __init__(a,m,u):
-  a.pin=m
-  a.sida=u
-  a.minst=1023
-  a.senast=0
-  a.dold_multi=1
-  a.uppdatera()
- def uppdatera(a):
-  a.senast=max(a.pin.read_analog(),1)
-  a.minst=min(a.minst,a.senast)
-  a.dold_multi=a.senast/a.minst
-R=Öga(mb.pin1,z)
-b=Öga(mb.pin2,w)
-O=0.05
-U=3 
-c=1.2 
-e=1000
-x=1.2
-o=300
-C=2000
+ def __init__(K,A,h):
+  K.pin=A
+  K.sida=h
+  K.minst=1023
+  K.up()
+ def up(K):
+  K.senast=max(K.pin.read_analog(),1)
+  K.minst=min(K.minst,K.senast)
+  K.dold_multi=K.senast/K.minst
+s=Öga(mb.pin1,l)
+u=Öga(mb.pin2,V)
+q=0.05
+m=3 
+w=1.2 
+i=2000
+f=1.8
+O=300
+P=500
 def hitta_mest_dolda():
- T=max(R.dold_multi,b.dold_multi)
- F=max(R.senast,b.senast)
- N=min(R.senast,b.senast)
- if max(F,1)/max(N,1)<c and T<U:
+ D=max(s.dold_multi,u.dold_multi)
+ M=max(s.senast,u.senast)
+ I=min(s.senast,u.senast)
+ if max(M,1)/max(I,1)<w and D<m:
   return None
- return R if R.senast>b.senast else b
+ return s if s.senast>u.senast else u
 class KraschHanterare:
- def __init__(a):
-  a.arr=[]
-  a.in_slot=0
-  a.minimum=-1
-  a.kraschar=False
- def reset(a):
-  a.arr.clear()
-  a.in_slot=0
-  a.minimum=-1
-  a.kraschar=False
- def uppdatera(a,tid):
-  if K.riktning!=s or K.sväng!=y: 
-   a.reset()
+ def __init__(K):
+  K.arr=[]
+  K.reset()
+ def reset(K):
+  K.arr.clear()
+  K.in_slot=0
+  K.minimum=-1
+  K.faktor=0
+ def up(K,tid):
+  if a.rik!=r or a.sväng!=T: 
+   K.reset()
    return
-  d=abs(mb.accelerometer.get_z())
-  if len(a.arr)!=0 and a.arr[-1][1]//100==tid//100:
-   a.arr[-1]=((a.arr[-1][0]*a.in_slot+d)/(a.in_slot+1),tid)
+  B=abs(mb.accelerometer.get_z())
+  if len(K.arr)!=0 and K.arr[-1][1]//200==tid//200:
+   K.arr[-1]=((K.arr[-1][0]*K.in_slot+B)/(K.in_slot+1),tid)
    return
-  a.arr.append((d,tid))
-  a.in_slot=1
-  i=False
-  while tid-a.arr[0][1]>e:
-   a.arr.pop(0)
-   i=True
-  if not i:
+  K.arr.append((B,tid))
+  K.in_slot=1
+  X=False
+  while tid-K.arr[0][1]>i:
+   K.arr.pop(0)
+   X=True
+  if not X:
    return
-  nu=a.genomsnitt()
-  a.minimum=nu if a.minimum==-1 else min(nu,a.minimum)
-  if nu/max(a.minimum,1)>x:
-   a.kraschar=True
- def genomsnitt(a):
-  t=0
-  for _,S in enumerate(a.arr):
-   t+=S[0]
-  return t/len(a.arr)
+  nu=K.genomsnitt()
+  K.minimum=nu if K.minimum==-1 else min(nu,K.minimum)
+  K.faktor=nu/max(K.minimum,1)
+  return K.faktor>f
+ def genomsnitt(K):
+  o=0
+  for _,n in enumerate(K.arr):
+   o+=n[0]
+  return o/len(K.arr)
+def sätt_pixlar(hur_många):
+ for n in range(25):
+  mb.display.set_pixel(n%5,n//5,9 if n<hur_många else 0)
 def hummux():
- h=False
+ v=False
  mb.pin12.write_digital(1)
- l=0
- L=Inställningar()
- W=Inställningar()
- P=False
- X=o
- Y=w
- G=KraschHanterare()
+ k=0
+ b=Inst()
+ p=Inst()
+ t=False
+ e=O
+ d=V
+ j=KraschHanterare()
  while True:
   if mb.button_a.was_pressed():
-   h=not h
-   P=False
+   v=not v
+   t=False
   if mb.button_b.was_pressed():
-   h=not h
-   P=True
-  if not h:
-   mb.display.show(mb.Image.SAD)
-   L.sätt(y,y)
-   flytta(L)
-   l=0
-   X=o
-   G.reset()
+   v=not v
+   t=True
+  if not v:
+   mb.display.show(IM.SAD)
+   b.sätt(T,T)
+   flytta(b)
+   k=0
+   e=O
+   j.reset()
    continue
-  g=mb.running_time()
-  R.uppdatera()
-  b.uppdatera()
-  W.sätt(s,y)
-  j=hitta_mest_dolda()
-  G.uppdatera(g)
-  if j is not None:
-   W.sväng=motsatt_sida(j.sida)
-  if G.kraschar:
-   W.sätt(r,Y)
-   L.sätt_från_annan(W)
-   l=0
-   X=C
-   Y=motsatt_sida(Y) 
-  if W.är_samma(L):
-   if g-l>X:
-    if not P:
-     flytta(W)
-    mb.display.show(W.bild())
+  W=mb.running_time()
+  s.up()
+  u.up()
+  p.sätt(r,T)
+  Y=hitta_mest_dolda()
+  J=j.up(W)
+  if J:
+   p.sätt(y,d)
+   b.sätt_från_annan(p)
+   k=0
+   e=P
+   d=motsatt_sida(d) 
+  if p.är_samma(b):
+   if W-k>e:
+    if not t:
+     flytta(p)
+    if not J:
+     e=O
   else:
-   l=g
-   L.sätt_från_annan(W)
+   k=W
+   b.sätt_från_annan(p)
+  sätt_pixlar(j.faktor*10)
 hummux()
--- test.py
+++ test.py
@@ -1,67 +1,54 @@
 import microbit as mb
 
 STANNA = 0
-FRAMÅT = 1
-BAKÅT = 2
-HÖGER = 1
-VÄNSTER = 2
+FRAMÅT = mb.pin13
+BAKÅT = mb.pin14
+HÖGER = mb.pin16
+VÄNSTER = mb.pin15
+IM = mb.Image
 
 
-class Inställningar:
+class Inst:
     def __init__(self):
-        self.riktning = STANNA
-        self.sväng = STANNA
+        self.sätt(STANNA, STANNA)
 
-    def sätt(self, riktning, sväng):
-        self.riktning = riktning
+    def sätt(self, rik, sväng):
+        self.rik = rik
         self.sväng = sväng
 
     def sätt_från_annan(self, annan):
-        self.sätt(annan.riktning, annan.sväng)
+        self.sätt(annan.rik, annan.sväng)
 
     def är_samma(self, annan):
-        return self.riktning == annan.riktning and self.sväng == annan.sväng
+        return self.rik == annan.rik and self.sväng == annan.sväng
 
     def bild(self):
+        if self.rik == BAKÅT:
+            return IM.NO
         if self.sväng == HÖGER:
-            return mb.Image.ARROW_W
+            return IM.ARROW_W
         if self.sväng == VÄNSTER:
-            return mb.Image.ARROW_E
-        return mb.Image.HEART
+            return IM.ARROW_E
+        return IM.HEART
 
 
-def sp(pin, val):
-    if pin.read_digital() != val:
-        pin.write_digital(val)
+def sp(val, pin):
+    pin.write_digital(1 if val == pin else 0)
 
 
 def motsatt_sida(sväng):
-    return VÄNSTER if sväng == HÖGER else VÄNSTER
+    return VÄNSTER if sväng == HÖGER else HÖGER
 
 
-nuvarande = Inställningar()
+nuvarande = Inst()
 
 
 def flytta(ins):
     nuvarande.sätt_från_annan(ins)
-    if ins.riktning == FRAMÅT:
-        sp(mb.pin13, 1)
-        sp(mb.pin14, 0)
-    elif ins.riktning == BAKÅT:
-        sp(mb.pin13, 0)
-        sp(mb.pin14, 1)
-    else:
-        sp(mb.pin13, 0)
-        sp(mb.pin14, 0)
-    if ins.sväng == HÖGER:
-        sp(mb.pin15, 0)
-        sp(mb.pin16, 1)
-    elif ins.sväng == VÄNSTER:
-        sp(mb.pin15, 1)
-        sp(mb.pin16, 0)
-    else:
-        sp(mb.pin15, 0)
-        sp(mb.pin16, 0)
+    sp(ins.rik, FRAMÅT)
+    sp(ins.rik, BAKÅT)
+    sp(ins.sväng, HÖGER)
+    sp(ins.sväng, VÄNSTER)
 
 
 class Öga:
@@ -69,11 +56,9 @@ class Öga:
         self.pin = pin
         self.sida = sida
         self.minst = 1023
-        self.senast = 0
-        self.dold_multi = 1
-        self.uppdatera()
+        self.up()
 
-    def uppdatera(self):
+    def up(self):
         self.senast = max(self.pin.read_analog(), 1)
         self.minst = min(self.minst, self.senast)
         self.dold_multi = self.senast / self.minst
@@ -84,10 +69,10 @@ vänster = Öga(mb.pin2, VÄNSTER)
 MIN_DOLD = 0.05
 MIN_DOLD_MULTI = 3  # hur mycket mer ett öga måste ha jämfört med min för att börja svänga
 MIN_DIFF = 1.2  # minsta skillnad mellan vänster/höger för att börja svänga
-KRASCH_TID_MÄTNING = 1000
-KRASCH_FAKTOR = 1.2
+KRASCH_TID_MÄTNING = 2000
+KRASCH_FAKTOR = 1.8
 TID_TILL_ÄNDRING = 300
-TID_TILL_SLUTA_RÄDDNING = 2000
+TID_TILL_SLUTA_RÄDDNING = 500
 
 
 def hitta_mest_dolda():
@@ -102,22 +87,20 @@ def hitta_mest_dolda():
 class KraschHanterare:
     def __init__(self):
         self.arr = []
-        #self.in_slot = 0
-        self.minimum = -1
-        self.kraschar = False
+        self.reset()
 
     def reset(self):
         self.arr.clear()
         self.in_slot = 0
         self.minimum = -1
-        self.kraschar = False
+        self.faktor = 0
 
-    def uppdatera(self, tid):
-        if nuvarande.riktning != FRAMÅT or nuvarande.sväng != STANNA:  # använd bara när vi går rakt framåt
+    def up(self, tid):
+        if nuvarande.rik != FRAMÅT or nuvarande.sväng != STANNA:  # använd bara när vi går rakt framåt
             self.reset()
             return
-        val = abs(mb.accelerometer.get_z())
-        if len(self.arr) != 0 and self.arr[-1][1] // 100 == tid // 100:
+        val = abs(mb.accelerometer.get_z()) # max(-mb.accelerometer.get_z(), 0)
+        if len(self.arr) != 0 and self.arr[-1][1] // 200 == tid // 200:
             self.arr[-1] = ((self.arr[-1][0] * self.in_slot + val) / (self.in_slot + 1), tid)
             return
         self.arr.append((val, tid))
@@ -130,8 +113,8 @@ class KraschHanterare:
             return
         nu = self.genomsnitt()
         self.minimum = nu if self.minimum == -1 else min(nu, self.minimum)
-        if nu / max(self.minimum, 1) > KRASCH_FAKTOR:
-            self.kraschar = True
+        self.faktor = nu / max(self.minimum, 1)
+        return self.faktor > KRASCH_FAKTOR
 
     def genomsnitt(self):
         totalt = 0
@@ -140,12 +123,17 @@ class KraschHanterare:
         return totalt / len(self.arr)
 
 
+def sätt_pixlar(hur_många):
+    for n in range(25):
+        mb.display.set_pixel(n % 5, n // 5, 9 if n < hur_många else 0)
+
+
 def hummux():
     är_startat = False
     mb.pin12.write_digital(1)
-    inställning_tid = 0
-    senaste_inställningarna = Inställningar()
-    nya_inställningar = Inställningar()
+    inst_tid = 0
+    senaste_inst = Inst()
+    nya_inst = Inst()
     stå_still = False
     till_ändring = TID_TILL_ÄNDRING
     räddning_håll = VÄNSTER
@@ -158,37 +146,41 @@ def hummux():
             är_startat = not är_startat
             stå_still = True
         if not är_startat:
-            mb.display.show(mb.Image.SAD)
-            senaste_inställningarna.sätt(STANNA, STANNA)
-            flytta(senaste_inställningarna)
-            inställning_tid = 0
+            mb.display.show(IM.SAD)
+            senaste_inst.sätt(STANNA, STANNA)
+            flytta(senaste_inst)
+            inst_tid = 0
             till_ändring = TID_TILL_ÄNDRING
             kraschen.reset()
             continue
         tiden = mb.running_time()
-        höger.uppdatera()
-        vänster.uppdatera()
-        nya_inställningar.sätt(FRAMÅT, STANNA)
+        höger.up()
+        vänster.up()
+        nya_inst.sätt(FRAMÅT, STANNA)
         mest_dolda = hitta_mest_dolda()
-        kraschen.uppdatera(tiden)
+        kraschar = kraschen.up(tiden)
 
-        if mest_dolda is not None:
-            nya_inställningar.sväng = motsatt_sida(mest_dolda.sida)
-        if kraschen.kraschar:
-            nya_inställningar.sätt(BAKÅT, räddning_håll)
-            senaste_inställningarna.sätt_från_annan(nya_inställningar)
-            inställning_tid = 0
+        # if mest_dolda is not None:
+        #    nya_inst.sväng = motsatt_sida(mest_dolda.sida)
+        if kraschar:
+            nya_inst.sätt(BAKÅT, räddning_håll)
+            senaste_inst.sätt_från_annan(nya_inst)
+            inst_tid = 0
             till_ändring = TID_TILL_SLUTA_RÄDDNING
             räddning_håll = motsatt_sida(räddning_håll)  # byt håll nästa gång
 
-        if nya_inställningar.är_samma(senaste_inställningarna):
-            if tiden - inställning_tid > till_ändring:
+        if nya_inst.är_samma(senaste_inst):
+            if tiden - inst_tid > till_ändring:
                 if not stå_still:
-                    flytta(nya_inställningar)
-                mb.display.show(nya_inställningar.bild())
+                    flytta(nya_inst)
+                mb.display.show(nya_inst.bild())
+                if not kraschar:
+                    till_ändring = TID_TILL_ÄNDRING
         else:
-            inställning_tid = tiden
-            senaste_inställningarna.sätt_från_annan(nya_inställningar)
+            inst_tid = tiden
+            senaste_inst.sätt_från_annan(nya_inst)
+        # sätt_pixlar(kraschen.faktor * 10)
+        # sätt_pixlar(-mb.accelerometer.get_y() / 40)
 
 
 hummux()
